f18d6826150c78af84d088966ab05c7e14252865
claudialandeirav <claudia.landeirav@gmail.com>
2023-10-19 19:40:41 +0200

    Documentation backend requests

--- web/backend/Character.py
+++ web/backend/Character.py
@@ -1,13 +1,37 @@
 from web.backend.Global import Global
 
+'''
+Class Character
+Author: Claudia Landeira
+
+It has the responsability of get the information about the character
+'''
 class Character:
 
+    '''
+    Function get_all
+    Author: Claudia Landeira
+
+    It has the responsability of get all character of Rick and Morty
+    '''
     def get_all(self):
         return Global.data(Global.getUrlCharacter())
     
+    '''
+    Function get
+    Author: Claudia Landeira
+
+    It has the responsability of get a character by id
+    '''
     def get(self, idCharacter):
         return Global.data(f"{Global.getUrlCharacter()}{idCharacter}")
     
+    '''
+    Function filter
+    Author: Claudia Landeira
+
+    It has the responsability of get the information filtering by name, status, species, type or gender
+    '''
     def filter(self, name=None, status=None, species=None, type=None, gender=None):        
         param = {}
 
@@ -29,6 +53,12 @@ class Character:
 
         return Global.data(url)
     
+    '''
+    Function info
+    Author: Claudia Landeira
+
+    It has the responsability of get info param on json data
+    '''
     def info(self, data):
         info = data['info']
         count = Character.count(info)
@@ -36,62 +66,137 @@ class Character:
 
         return [count, pages]
     
-    def count(self, data):
-        return data['count']
+    '''
+    Function origin
+    Author: Claudia Landeira
+
+    It has the responsability of get origin param of info param on json data
+    '''
+    def origin(self, data):
+        origin = data['origin']
+        name = Character.name(origin)
+        url = Character.url(origin)
+
+        return [name, url]
     
-    def pages(self, data):
-        return data['pages']
-    
+    '''
+    Function location
+    Author: Claudia Landeira
+
+    It has the responsability of get location param of info param on json data
+    '''
+    def location(self, data):
+        location = data['location']
+        name = Character.name(location)
+        url = Character.url(location)
+
+        return [name, url]
+        
+    '''
+    Function results
+    Author: Claudia Landeira
+
+    It has the responsability of get results param of info param on json data
+    '''
     def results(self, data):
         return data['results']
     
+    '''
+    Function count
+    Author: Claudia Landeira
+
+    It has the responsability of get count param of info param on json data
+    '''
+    def count(self, data):
+        return data['count']
+    
+    '''
+    Function pages
+    Author: Claudia Landeira
+
+    It has the responsability of get pages param of info param on json data
+    '''
+    def pages(self, data):
+        return data['pages']
+    
+    '''
+    Function id
+    Author: Claudia Landeira
+
+    It has the responsability of get id param of info param on json data
+    '''
     def id(self, data):
         return data['id']
     
+    '''
+    Function name
+    Author: Claudia Landeira
+
+    It has the responsability of get name param of info param on json data
+    '''
     def name(self, data):
         return data['name']
     
+    '''
+    Function url
+    Author: Claudia Landeira
+
+    It has the responsability of get url param of info param on json data
+    '''
+    def url(self, data):
+        return data['url']
+    
+    '''
+    Function status
+    Author: Claudia Landeira
+
+    It has the responsability of get status param of info param on json data
+    '''
     def status(self, data):
         return data['status']
     
+    '''
+    Function species
+    Author: Claudia Landeira
+
+    It has the responsability of get species param of info param on json data
+    '''
     def species(self, data):
         return data['species']
     
+    '''
+    Function type
+    Author: Claudia Landeira
+
+    It has the responsability of get type param of info param on json data
+    '''
     def type(self, data):
         return data['type']
     
+    '''
+    Function gender
+    Author: Claudia Landeira
+
+    It has the responsability of get gender param of info param on json data
+    '''
     def gender(self, data):
         return data['gender']
     
-    def origin(self, data):
-        origin = data['origin']
-        name = Character.nameOrigin(origin)
-        url = Character.urlOrigin(origin)
+    '''
+    Function image
+    Author: Claudia Landeira
 
-        return [name, url]
-    
-    def nameOrigin(self, data):
-        return data['name']
-    
-    def urlOrigin(self, data):
-        return data['url']
-    
-    def location(self, data):
-        location = data['location']
-        name = Character.nameLocation(location)
-        url = Character.urlLocation(location)
-
-        return [name, url]
-    
-    def nameLocation(self, data):
-        return data['name']
-    
-    def urlLocation(self, data):
-        return data['url']
-    
+    It has the responsability of get image param of info param on json data
+    '''    
     def image(self, data):
         return data['image']
     
+    '''
+    Function episode
+    Author: Claudia Landeira
+
+    It has the responsability of get episode param of info param on json data
+    '''
     def episode(self, data):
         return data['episode']
     
--- web/backend/Episode.py
+++ web/backend/Episode.py
@@ -1,13 +1,37 @@
 from web.backend.Global import Global
 
+'''
+Class Episode
+Author: Claudia Landeira
+
+It has the responsability of get the information about the episode
+'''
 class Episode:
 
+    '''
+    Function get_all
+    Author: Claudia Landeira
+
+    It has the responsability of get all character of Rick and Morty
+    '''
     def get_all(self):
         return Global.data(Global.getUrlEpisode())
     
+    '''
+    Function get
+    Author: Claudia Landeira
+
+    It has the responsability of get a character by id
+    '''
     def get(self, idEpisode):
         return Global.data(f'{Global.getUrlEpisode()}{idEpisode}')
     
+    '''
+    Function filter
+    Author: Claudia Landeira
+
+    It has the responsability of get the information filtering by name, status, species, type or gender
+    '''
     def filter(self, name=None, air_date=None, episode=None):
         param = {}
 
@@ -25,6 +49,12 @@ class Episode:
 
         return Global.data(url)
     
+    '''
+    Function info
+    Author: Claudia Landeira
+
+    It has the responsability of get info param on json data
+    '''
     def info(self, data):
         info = data['info']
         count = Episode.count(info)
@@ -32,26 +62,74 @@ class Episode:
         
         return [count, pages]
     
+    '''
+    Function count
+    Author: Claudia Landeira
+
+    It has the responsability of get count param of info param on json data
+    '''
     def count(self, data):
         return data['count']
     
+    '''
+    Function pages
+    Author: Claudia Landeira
+
+    It has the responsability of get pages param of info param on json data
+    '''
     def pages(self, data):
         return data['pages']
     
+    '''
+    Function results
+    Author: Claudia Landeira
+
+    It has the responsability of get results param of info param on json data
+    '''
     def results(self, data):
         return data['results']
     
+    '''
+    Function id
+    Author: Claudia Landeira
+
+    It has the responsability of get id param of info param on json data
+    '''
     def id(self, data):
         return data['id']
     
+    '''
+    Function name
+    Author: Claudia Landeira
+
+    It has the responsability of get name param of info param on json data
+    '''
     def name(self, data):
         return data['name']
     
+    '''
+    Function air_date
+    Author: Claudia Landeira
+
+    It has the responsability of get air date param of info param on json data
+    '''
     def air_date(self, data):
         return data['air_date']
     
+    '''
+    Function episode
+    Author: Claudia Landeira
+
+    It has the responsability of get episode param of info param on json data
+    '''
     def episode(self, data):
         return data['episode']
     
+    '''
+    Function characters
+    Author: Claudia Landeira
+
+    It has the responsability of get characters param of info param on json data
+    '''
     def characters(self, data):
         return data['characters']
