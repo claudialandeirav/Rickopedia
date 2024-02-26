49b3d2f9ef81950bd99f6676c815a21b43808dd0
Claudia Landeira Viñuela <144339936+claudialandeirav@users.noreply.github.com>
2024-02-26 12:11:57 +0100

    F0007_B0004-Localizaciones (#46)

--- web/backend/Location.py
+++ web/backend/Location.py
@@ -1,4 +1,9 @@
 from web.backend.Global import Global
+import networkx as nx
+import random 
+import matplotlib.pyplot as plt
+import io
+import base64
 
 '''
 Clase Location
@@ -9,13 +14,13 @@ Informacion de las localizaciones
 class Location:
     
     '''
-    Funcion get_all
+    Funcion getAllPaged
     Autora: Claudia Landeira
 
-    Devuelve toda la informacion de las localizaciones disponibles
+    Devuelve todos las localizacinos disponibles y sus datos paginados
     '''
-    def get_all():
-        return Global.data(Global.getUrlLocation())
+    def getAllPaged(page):
+        return Global.data(f"{Global.getUrlLocation()}?page={page}")
     
     '''
     Funcion get
@@ -26,6 +31,15 @@ class Location:
     def getById(idLocation):
         return Global.data(f'{Global.getUrlLocation()}{idLocation}')
     
+    '''
+    Funcion getByList
+    Autora: Claudia Landeira
+
+    Devuelve toda la informacion por lista
+    '''
+    def getByList(lista):
+        return Global.data(f'{Global.getUrlLocation()}{lista}')
+
     '''
     Funcion filter
     Autora: Claudia Landeira
@@ -59,7 +73,7 @@ class Location:
         count = Location.count(info)
         pages = Location.pages(info)
         
-        return [count, pages]
+        return {'count': count, 'pages': pages}
     
     '''
     Funcion count
@@ -132,3 +146,74 @@ class Location:
     '''
     def residents(data):
         return data['residents']
+    
+    '''
+    Funcion createGraph
+    Autora: Claudia Landeira
+
+    Devuelve un grafo de tipo NetworkX con las localizaciones
+    '''
+    def createGraph (locations):
+        graph = nx.Graph()
+
+        for location in locations:
+            graph.add_node(location['name'])
+
+        connections = set()
+        max_connections = len(graph.nodes()) * (len(graph.nodes()) - 1) // 2
+
+        for _ in range(max_connections):
+            node1 = random.choice(list(graph.nodes()))
+            node2 = random.choice(list(graph.nodes()))
+
+            if node1 != node2 and (node1, node2) not in connections and random.random() < 0.2:
+                graph.add_edge(node1, node2)
+                connections.add((node1, node2))
+                connections.add((node2, node1))
+
+                if len(connections) == len(graph.nodes()) - 1:
+                    break
+
+        return graph
+    
+    '''
+    Funcion createImage
+    Autora: Claudia Landeira
+
+    Devuelve una imagen con el grafo dado por parametros
+    '''
+    def createImage(graph):
+        pos = nx.shell_layout(graph)
+
+        plt.figure(figsize=(14, 7))
+
+        nx.draw_networkx_nodes(graph, pos, node_size=700, node_color="skyblue")
+        nx.draw_networkx_labels(graph, pos, font_size=10, font_weight="bold")
+        
+        for edge in graph.edges():
+            nx.draw_networkx_edges(graph, pos, arrows=True, edgelist=[edge], connectionstyle="arc3,rad=0.3", alpha=0.5)
+
+        plt.axis('off')
+
+        img = io.BytesIO()
+
+        plt.savefig(img, format='png')
+        img.seek(0)
+        image_base64 = base64.b64encode(img.getvalue()).decode()
+
+        return image_base64
+    
+    '''
+    Funcion nodesLinked
+    Autora: Claudia Landeira
+
+    Devuelve una imagen con enlaces asignados
+    '''
+    def nodesLinked(graph):
+        linked = []
+
+        for node in graph.nodes():
+            link = f'<a href="/localizacion/{node}">{{ node }}</a>'
+            linked.append(link)
+        return linked
+    
